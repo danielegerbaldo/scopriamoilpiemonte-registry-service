6c23050c05b170d9f104a5aa43897da00650e10b
danielegerbaldo <daniele.gerbaldo@gmail.com>
2021-05-20 17:40:14 +0200

    Source added

---
+++ src/test/java/TAASS/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package TAASS.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
